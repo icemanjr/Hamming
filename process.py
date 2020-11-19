71c4f1e283ca96b9a568d1d019bc4f890ee9fadc
Icemanjr <holst.nathan@gmail.com>
2020-11-19 12:45:22 -0700

    Added a processing.py file, this will be the program that will take a .txt file, feed it to my Hamming program. Pretend to transfer that file, which will corrupt some of the data, then using the Hamming program, fix the file back to its original state.

---
+++ process.py
@@ -0,0 +1,6 @@
+# To bring a file and add a Hamming ECC to be ready for transfer.
+# Then to corrupt a single bit per line, to simulate data corruption during transfer.
+# Then to make a copy without the ECC to show the corrupt file.
+# Then to fix the file using the Hamming ECC. Ending with 4 different files.
+# 1st the file_transfer.txt, 2nd the transfer_corrupt.txt, 3rd the corrupt.txt, and 4th the file_fixed.txt
+
